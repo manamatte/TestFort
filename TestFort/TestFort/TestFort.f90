469ccd897e98a00382997d1afd687e22201a8d6c
MatteMa <manamatte@gmail.com>
2021-04-09 13:54:03 +0200

    first TestFor commit

---
+++ TestFort/TestFort/TestFort.f90
@@ -0,0 +1,25 @@
+!  TestFort.f90 
+!
+!  FUNCTIONS:
+!  TestFort - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: TestFort
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program TestFort
+
+    implicit none
+
+    ! Variables
+
+    ! Body of TestFort
+    print *, 'Hello World'
+
+    end program TestFort
+
